70ac9df8001ab1e02256e38c45772c621ae55a88
TrinhDinhPhuc <phuccoi996@gmail.com>
2018-05-09 14:04:56 +0700

    gradient decent

--- One-hot-encoding.py
+++ One-hot-encoding.py
@@ -1,15 +1,13 @@
 from numpy import argmax
-# define input string
+# khởi tạo chuỗi đầu vào
 data = 'xin chào'
-# define universe of possible input values
-# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# định nghĩa bộ chữ cái
 alphabet='aăàáâbcdđeêghiklmnoôơpqrxtuưvsy '
-# define a mapping of chars to integers
+# mapping chuỗi sang integer
 char_to_int = dict((c, i) for i, c in enumerate(alphabet))
 int_to_char = dict((i, c) for i, c in enumerate(alphabet))
-# Integer encode input data
 integer_encoded = [char_to_int[char] for char in data]
-print(integer_encoded)
+print("Giá trị encode của chữ x : {}".format(integer_encoded[0]))
 # one hot encoding
 onehot_encoded = list()
 for value in integer_encoded:
@@ -17,5 +15,6 @@ for value in integer_encoded:
 	letter[value] = 1
 	onehot_encoded.append(letter)
 print(onehot_encoded)
-# invert encoding
+# trả lại giá trị sau mã hóa, ở đây ta sẽ ví dụ ta muốn lấy chữ i ở vị trí =1 trong chuỗi "xin chào" ban đầu
 inverted = int_to_char[argmax(onehot_encoded[1])]
+print("Giải mã: {}".format(inverted))
